55a03b3c847de3dae25101d8e561f3cc6ee38637
Intjar Mansuri <intjar1606@gmail.com>
2024-10-11 16:30:28 +0530

    Min Heap

---
+++ 1942. The Number of the Smallest Unoccupied Chair.cpp
@@ -0,0 +1,86 @@
+/* --------------- Approach-1 : Brutforce 
+Time Complexity : O(n^2)
+Space Complexity : O(N) --------------- */
+
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int n = times.size();
+
+        vector<int> chairs(n, -1);
+        int targetFriendArrivalTime = times[targetFriend][0];
+        sort(begin(times), end(times));
+
+        for(vector<int>& time : times) {
+            int arrival = time[0];
+            int depart = time[1];
+
+            for(int i = 0; i < n; i++) {
+                if(chairs[i] <= arrival) {
+                    chairs[i] = depart;
+
+                    if(arrival == targetFriendArrivalTime) {
+                        return i;
+                    }
+                    break;
+                }
+            }
+        }
+        return -1;
+
+    }
+};
+
+/* --------------- Approach-2 : Using minHeap
+Time Complexity : O(NlogN)
+Space Complexity : O(N) --------------- */
+
+
+class Solution {
+public:
+    typedef pair<int, int> P;
+
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        
+        int n = times.size();
+        priority_queue<P, vector<P>, greater<P> > occupied; //{departTime, chairNo}
+        priority_queue<int, vector<int>, greater<int>> free; //min heap of unoccupied chairs
+        
+        int targetFriendArrival = times[targetFriend][0];
+		
+        //Sort based on arrival time
+        sort(times.begin(),times.end());
+        
+		int chairNo = 0;
+
+        for(int i = 0; i < n; i++) {
+            int arrival  = times[i][0];
+            int depart   = times[i][1];
+            
+            //free chairs accordingly
+            while(!occupied.empty() && occupied.top().first <= arrival) {
+                free.push(occupied.top().second); //this chair is now free
+                occupied.pop();
+            }
+
+            if(free.empty()) {
+                occupied.push({depart, chairNo});
+
+                if(arrival == targetFriendArrival)
+                    return chairNo;
+
+                chairNo++;
+            } else {
+                int leastChairAvailable = free.top();
+                free.pop();
+                if(arrival == targetFriendArrival) {
+                    return leastChairAvailable;
+                }
+                occupied.push({depart, leastChairAvailable});
+            }
+        }
+
+        return -1;
+    }
+    
+};
