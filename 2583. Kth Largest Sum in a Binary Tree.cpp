42ece1f4a61c4d7a4b96df1f5f379c8f04b31117
Intjar Mansuri <intjar1606@gmail.com>
2024-10-22 20:31:27 +0530

    Priority Queue

---
+++ 2583. Kth Largest Sum in a Binary Tree.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long kthLargestLevelSum(TreeNode* root, int k) {
+        priority_queue<long, vector<long>, greater<long>> pq; //Min Heap
+
+        queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty()) {
+            int n    = que.size();
+            long levelSum = 0;
+            while (n--) {
+                TreeNode* poppedNode = que.front();
+                que.pop();
+                levelSum += poppedNode->val;
+                if (poppedNode->left != NULL) {
+                    que.push(poppedNode->left);
+                }
+                if (poppedNode->right != NULL) {
+                    que.push(poppedNode->right);
+                }
+            }
+
+            pq.push(levelSum);
+
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        return pq.size() < k ? -1 : pq.top();
+    }
+};
