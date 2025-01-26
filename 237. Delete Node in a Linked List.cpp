c6c11a57d7df0d3be64f044651f820608ffa5bc3
Intjar Mansuri <intjar1606@gmail.com>
2025-01-26 19:26:36 +0530

    Linked List

---
+++ 237. Delete Node in a Linked List.cpp
@@ -0,0 +1,17 @@
+// Time Complexity : O(N)
+
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* prev = NULL;
+
+        while(node != NULL && node->next != NULL) {
+            node->val = node->next->val;
+            prev = node;
+            node = node->next;
+        }
+        prev->next = NULL;
+        delete(node);
+    }
+};
